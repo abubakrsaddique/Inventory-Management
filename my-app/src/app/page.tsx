962249a9fe4784d2f53a4dbca6bce0f60b630659
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-09-04 21:27:37 +0500

    Navbar Added

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Dashboard />
     </div>
   );
 }
