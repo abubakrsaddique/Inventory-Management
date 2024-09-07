b250fc70f63c50041a719ad314f50394f6c61e30
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-09-07 10:55:37 +0500

    dashboard route added

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import dashboardRoutes from "./routes/dashboardRoutes";
 /* ROUTE IMPORTS */
 // import dashboardRoutes from "./routes/dashboardRoutes";
 // import productRoutes from "./routes/productRoutes";
@@ -22,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-//
+app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
